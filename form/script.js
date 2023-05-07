712cb6772ea78fa473bfc5c1ede0a7cb04f1175d
tompraxenthaler <130749500+tompraxenthaler@users.noreply.github.com>
2023-05-07 20:27:08 +0200

    Add files via upload

--- form/script.js
+++ form/script.js
@@ -20,7 +20,10 @@ function sendMail () {
         console.log(parameters);
     var optIn = document.getElementById("optin");
         console.log("OptIn:" + optIn.checked);
-    
+
+    ////////////////////////////////////////////
+    // EmailJS trigger
+    ////////////////////////////////////////////
     const serviceId = "service_vawz19v";
     const templateId = "template_cjv4nfe";
     const publicKey = "shonANTuR2xK5mQ0Q";
@@ -39,12 +42,47 @@ function sendMail () {
         }
         );
 
-        if (optIn.checked) {
-            console.log("OptIn");
-            sendCleverReachData();
-        };
+    ////////////////////////////////////////////
+    // Bei Opt-in CleverReach Funktion ausführen 
+    ////////////////////////////////////////////
+    if (optIn.checked) {
+        console.log("OptIn");
+        sendCleverReachData();
+    };
+
+    ///////////////////////////
+    // Leeren der Input-Felder 
+    ///////////////////////////
+    document.getElementById("anrede").value = "";
+    document.getElementById("titel").value = "";
+    document.getElementById("vorname").value = "";
+    document.getElementById("nachname").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("optin").checked = false;
+    document.getElementById("strasse").value = "";
+    document.getElementById("hausnummer").value = "";
+    document.getElementById("adresszusatz").value = "";
+    document.getElementById("plz").value = "";
+    document.getElementById("ort").value = "";
+    document.getElementById("land").value = "";
+    document.getElementById("telefon").value = "";
+    document.getElementById("geburtstag").value = "";
+    var sportartCheckboxes = document.getElementsByClassName("sportart-checkbox");
+    for (var i = 0; i < sportartCheckboxes.length; i++) {
+        sportartCheckboxes[i].checked = false;
+    }
+    document.getElementById("consent").checked = false;
+
+    ////////////////////////////////////////
+    // Öffnen der Success Page nach Absenden
+    ////////////////////////////////////////
+    var newWindow = window.open("", "_blank");
+    newWindow.location.href = "success.html";
 };
 
+/////////////////////////////////////////////
+// Funktion: Daten an CleverReach übermitteln
+/////////////////////////////////////////////
 function sendCleverReachData() {  
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
